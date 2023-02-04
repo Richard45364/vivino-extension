12c6ee6333d8326b7f974b5c176128ef58cd53c7
Rickard Johansson <rickard.haldor@gmail.com>
2023-02-04 14:09:23 +0100

    wip: get wine title, etc from systembolaget

---
+++ script.js
@@ -0,0 +1,17 @@
+
+main(document.body)
+
+function main(documentBody){
+    const wine = documentBody.getElementsByClassName("css-dahppd e3whs8q0")
+    console.log(wine)
+}
+
+// Finds name of wine
+function findName() {
+
+}
+
+// Finds year of wine
+function findYear() {
+
+}
