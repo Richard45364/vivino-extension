eef80c221b4d835295be53aa9df85c22c9fba022
Rickard Johansson <rickard.haldor@gmail.com>
2023-02-04 16:05:29 +0100

    fixed code separation

--- script.js
+++ script.js
@@ -2,22 +2,19 @@
 main(document.body)
 
 function main(documentBody){
-    const wineNameObj = documentBody.getElementsByClassName("css-1rw23u7 enp2lf70")
-    const wineYearObj = documentBody.getElementsByClassName("css-qry54b enp2lf70")
-
-    wineName = getName(wineNameObj)
-    wineYear = getYear(wineYearObj)
+    wineName = getName(documentBody)
+    wineYear = getYear(documentBody)
 
     console.log(wineName)
     console.log(wineYear)
 }
 
 // Finds name of wine
-function getName(wineNameObj) {
-    return wineNameObj[0].innerText
+function getName(documentBody) {
+    return documentBody.getElementsByClassName("css-1rw23u7 enp2lf70")[0].innerText
 }
 
 // Finds year of wine
-function getYear(wineYearObj) {
-    return wineYearObj[0].innerText
+function getYear(documentBody) {
+    return documentBody.getElementsByClassName("css-qry54b enp2lf70")[0].innerText
 }
